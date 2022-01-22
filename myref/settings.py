2918548f06a68afd7b72e28d71c1ed8de8b34aba
kwon <yz3816@naver.com>
2022-01-22 21:35:26 +0900

    로깅 추가

--- myref/settings.py
+++ myref/settings.py
@@ -190,3 +190,20 @@ CORS_ALLOW_HEADERS = (
     'x-requested-with',
 )
 
+# sql 로그 보기 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
