9a3a28af29675181e15ea93cd40d61b7bc875d0b
kwon <yz3816@naver.com>
2022-01-23 21:01:38 +0900

    0901

--- ref/urls.py
+++ ref/urls.py
@@ -5,5 +5,7 @@ app_name = 'ref'
 urlpatterns = [
 
     path('main/', main_views.main, name='main'),
+    path('main/searchRecipe/', main_views.searchRecipe, name='searchRecipe'),
+    path('main/searchRecipe/moreNeed/', main_views.moreNeed, name='moreNeed'),
 
 ]
--- ref/views.py
+++ ref/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from .models import Recipe, UserRef
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def main(request):
 
     recipe_list = Recipe.objects.order_by('rcp_sno')[0:5]
     userref_list = UserRef.objects.all()
 
     return render(request,'ref/main.html',{'recipe_list':recipe_list,'userref_list':userref_list})
+
+@csrf_exempt
+def searchRecipe(request):
+
+    # 처리 필요
+    # 내 냉장고 레시피의 재료들과
+    # 레시피 테이블에서의 검색 결과를 조합
+    # 내가 가지고 있는 재료들과 완벽히 일치하는 리스트
+    # 더보기 에는 추가 재료 필요한 것
+
+    recipe_list = Recipe.objects.all()[0:5]
+    userref_list = UserRef.objects.all()
+
+    return render(request,'ref/searchRecipe.html',{'recipe_list':recipe_list})
+
+@csrf_exempt
+def moreNeed(request):
+
+
+
+    return render(request,'ref/moreNeed.html',{})
