009d8671f0af368f6a0741a2a807a87e6ee4a60b
yoonkyeong <dbsrud5916@gmail.com>
2022-01-23 19:25:34 +0900

    main_page_update

--- ref/views.py
+++ ref/views.py
@@ -4,6 +4,7 @@ from .models import Recipe
 # Create your views here.
 def main(request):
 
-    recipe_list = Recipe.objects.order_by('rcp_sno')[0:5]
+    recipe_list = Recipe.objects.order_by('rcp_sno')[0:1]
 
     return render(request,'ref/main.html',{'recipe_list':recipe_list})
+
